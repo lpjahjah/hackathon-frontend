a274932eba970f58878e70c39ddb82f99d8e61dc
Gabriel <gabrielferrari601@gmail.com>
2022-11-07 15:26:06 -0300

    feat(home): altera texto de apresentação e remove botão

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,43 +1,30 @@
-import { useNavigate } from 'react-router-dom';
 import TrackCard from '../../components/TrackCard';
 import style from './style.module.css';
 
-const Home = () => {
-  const navigate = useNavigate();
-
-  const navigateToLogin = () => {
-    navigate('/login');
-  };
-  return (
-    <section>
-      <section className={style.firstSection}>
-        <div>
-          <h1 className="title-b-l">
-            Vamos embarcar juntos nessa jornada?
-          </h1>
-          <p className="label-r-m">
-            Se você procura conteúdo tech de qualidade e gratuito, O Orange Evolution é
-            {' '}
-            <strong>para você</strong>
-            ,
-            pode chegar!
-          </p>
-        </div>
-        <div>
-          <button type="button" onClick={navigateToLogin}> Começar</button>
-        </div>
-      </section>
-      <section>
-        <h2>Escolha quantas trilhas quiser:</h2>
-        <div className={style.cardsContainer}>
-          <TrackCard title="Desenvolvedor Full Stack" />
-          <TrackCard title="UX/UI Designer" />
-          <TrackCard title="Quality Assurance(QA)" />
-        </div>
-      </section>
+const Home = () => (
+  <section>
+    <section className={style.firstSection}>
+      <div className={style.descriptionContainer}>
+        <h1 className="title-b-l">
+          Olá, seja bem vindo!
+        </h1>
+        <p className="label-r-sm">
+          Nossas trilhas foram montadas pensando em quem está começando na área,
+          ou passando por uma migração de carreira e ainda não sabe exatamente o
+          que é esse mundo. Então, aperta o cinto e vem com a gente nessa jornada!
+        </p>
+      </div>
     </section>
+    <section>
+      <h2 className="label-uppercase">Escolha quantas trilhas quiser:</h2>
+      <div className={style.cardsContainer}>
+        <TrackCard title="Desenvolvedor Full Stack" trackPath="/developer" />
+        <TrackCard title="UX/UI Designer" trackPath="/uxuidesigner" />
+        <TrackCard title="Quality Assurance(QA)" trackPath="/qa" />
+      </div>
+    </section>
+  </section>
 
-  );
-};
+);
 
 export default Home;
