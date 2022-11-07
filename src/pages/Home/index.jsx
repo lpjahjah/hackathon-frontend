03589c38f7bfd13b631b4399106e56feff2b43b4
Gabriel <gabrielferrari601@gmail.com>
2022-11-07 13:24:47 -0300

    feat(home): :construction: cria principais informaçoes da tela de onboarding

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,43 @@
-import './style.css';
+import { useNavigate } from 'react-router-dom';
+import TrackCard from '../../components/TrackCard/TrackCard';
+import style from './style.module.css';
 
-const Home = () => (
-  <div>
-    <h1>Home</h1>
-  </div>
-);
+const Home = () => {
+  const navigate = useNavigate();
+
+  const navigateToLogin = () => {
+    navigate('/login');
+  };
+  return (
+    <section>
+      <section className={style.firstSection}>
+        <div>
+          <h1 className="title-b-l">
+            Vamos embarcar juntos nessa jornada?
+          </h1>
+          <p className="label-r-m">
+            Se você procura conteúdo tech de qualidade e gratuito, O Orange Evolution é
+            {' '}
+            <strong>para você</strong>
+            ,
+            pode chegar!
+          </p>
+        </div>
+        <div>
+          <button type="button" onClick={navigateToLogin}> Começar</button>
+        </div>
+      </section>
+      <section>
+        <h2>Escolha quantas trilhas quiser:</h2>
+        <div className={style.cardsContainer}>
+          <TrackCard title="Desenvolvedor Full Stack" />
+          <TrackCard title="UX/UI Designer" />
+          <TrackCard title="Quality Assurance(QA)" />
+        </div>
+      </section>
+    </section>
+
+  );
+};
 
 export default Home;
