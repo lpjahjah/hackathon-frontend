cde19d38b1ce66c8b26ea8c39a7abea2f9936e17
lpjahjah <lp.borgomoni@gmail.com>
2022-11-10 22:50:39 -0300

    created PageHeaderText

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,21 +4,18 @@ import TracksEnum from '../../enums/TracksEnum';
 import devLogo from '../../images/settings.png';
 import uxLogo from '../../images/graphic-design.png';
 import qaLogo from '../../images/search.png';
+import Page from '../../components/Page';
+import PageHeaderText from '../../components/PageHeaderText';
 
 const Home = () => (
-  <div className="container">
-    <div className="container--header--text">
-      <div className={style.descriptionContainer}>
-        <h1>
-          Olá, seja bem vindo!
-        </h1>
-        <p>
-          Nossas trilhas foram montadas pensando em quem está começando na área,
-          ou passando por uma migração de carreira e ainda não sabe exatamente o
-          que é esse mundo. Então, aperta o cinto e vem com a gente nessa jornada!
-        </p>
-      </div>
-    </div>
+  <Page>
+    <PageHeaderText
+      title="Olá, seja bem vindo!"
+      text="Nossas trilhas foram montadas pensando em quem está começando na área,
+      ou passando por uma migração de carreira e ainda não sabe exatamente o
+      que é esse mundo. Então, aperta o cinto e vem com a gente nessa jornada!"
+    />
+
     <section>
       <div className={style.cardsContainer}>
         <TrackCard title="Desenvolvedor Full Stack" track={TracksEnum.fullstack} image={devLogo} />
@@ -26,7 +23,7 @@ const Home = () => (
         <TrackCard title="Quality Assurance(QA)" track={TracksEnum.qa} image={qaLogo} />
       </div>
     </section>
-  </div>
+  </Page>
 );
 
 export default Home;
