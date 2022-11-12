4bb8fc36574dec79bcbc1e3ceb88efc7abf18aa8
lpjahjah <lp.borgomoni@gmail.com>
2022-11-12 01:04:42 -0300

    pageHeaderText component now with subtitle and first letter capitalized

--- src/components/PageHeaderText/index.jsx
+++ src/components/PageHeaderText/index.jsx
@@ -1,11 +1,17 @@
+/* eslint-disable react/require-default-props */
 import PropTypes from 'prop-types';
 
-const PageHeaderText = ({ title, text }) => (
+const PageHeaderText = ({ title, text, subtitle }) => (
   <div className="container__header__text">
     <div className="container__header__text__description">
       <h1>
         {title}
       </h1>
+      {subtitle && (
+        <h2 className="container__header__text__description__subtitle">
+          {subtitle}
+        </h2>
+      )}
       <p>
         {text}
       </p>
@@ -16,6 +22,7 @@ const PageHeaderText = ({ title, text }) => (
 PageHeaderText.propTypes = {
   title: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
 };
 
 export default PageHeaderText;
