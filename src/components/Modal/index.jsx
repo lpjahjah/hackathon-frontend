900a9e31b8dca919dfd9d71fb76dde429386ff99
lpjahjah <lp.borgomoni@gmail.com>
2022-11-14 19:21:34 -0300

    modal basic component created

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/require-default-props */
+import { Close } from '@mui/icons-material';
+import PropTypes from 'prop-types';
+import { useEffect, useCallback, useRef } from 'react';
+import './style.css';
+
+const Modal = ({ open, setOpen, title, children }) => {
+  const modalRef = useRef();
+
+  useEffect(() => {
+    if (open)
+      modalRef.current.classList.toggle('show');
+    else
+      modalRef.current.classList.remove('show');
+  }, [open]);
+
+  const toggleModal = useCallback(() => {
+    setOpen(!open);
+  }, [open, setOpen]);
+
+  return (
+    <div ref={modalRef} className="modal" aria-hidden="true">
+
+      <div className="modal__content">
+        <div className="modal__header">
+          <h2>{title}</h2>
+          <div className="modal__header__close_button" onClick={toggleModal} aria-hidden="true">
+            <Close />
+          </div>
+        </div>
+
+        <div className="modal__body">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  open: PropTypes.bool.isRequired,
+  setOpen: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node,
+};
+
+export default Modal;
