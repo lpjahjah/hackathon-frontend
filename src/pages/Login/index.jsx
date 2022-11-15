900178eabf8e8fbf898b55ccb456644bcdf5c42d
alvesgf16 <alvesgabriel1994@gmail.com>
2022-11-14 21:59:54 -0300

    refactor: create component for login header

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,32 +1,12 @@
-import LoginForm from '../../components/LoginForm';
+import LoginForm from './LoginForm';
+import LoginHeaderText from './LoginHeaderText';
 import style from './style.module.css';
 import foguete from '../../images/foguete.svg';
-
-const introText = (
-  <header className={style['login-header']}>
-    <h1>Evolua a sua carreira na tecnologia</h1>
-    <p>Explore conhecimentos que estão transformando</p>
-    <p>indústrias, negócios e vidas através de trilhas</p>
-    <p>
-      gratuitas em
-      {' '}
-      <strong>Desenvolvimento</strong>
-      ,
-      {' '}
-      <strong>UX/UI Design</strong>
-      {' '}
-      e
-      {' '}
-      <strong>QA</strong>
-      !
-    </p>
-  </header>
-);
 
 const Login = () => (
   <div className={style['page-structure']}>
     <main className={style['login-main']}>
-      {introText}
+      <LoginHeaderText />
       <LoginForm />
     </main>
     <aside className={style['login-side']}>
